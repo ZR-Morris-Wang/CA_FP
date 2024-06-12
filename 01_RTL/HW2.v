cefae09f09b5ef6040e2b33c2ab44f14268fcfe6
ZR-Morris-Wang <ppmmlzr@gmail.com>
2024-06-12 20:45:53 +0800

    Updated to correct file

--- 01_RTL/HW2.v
+++ 01_RTL/HW2.v
@@ -36,12 +36,15 @@ module ALU #(
 // Wires & Regs
     // Todo
     // state
-    reg  [         4: 0] state, state_nxt; // remember to expand the bit width if you want to add more states!
+    reg  [         3: 0] state, state_nxt; // remember to expand the bit width if you want to add more states!
     // load input
     reg  [  DATA_W-1: 0] operand_a, operand_a_nxt;
     reg  [  DATA_W-1: 0] operand_b, operand_b_nxt;
     reg  [         2: 0] inst, inst_nxt;
-    reg  [2*DATA_W-1: 0] result, result_nxt;
+    reg  [  2*DATA_W: 0] result, result_nxt;
+    reg  [2*DATA_W-1: 0] temp;
+    reg  [  2*DATA_W: 0] divTemp;
+    reg  [         4: 0] counter, counter_nxt;
     reg valid_signal, valid_signal_nxt;
 // Wire Assignments
     // Todo
@@ -69,34 +72,135 @@ module ALU #(
                 if(!i_valid) begin
                     state_nxt = S_IDLE;
                 end
-                state_nxt = {0, inst};
+                else begin
+                    case(i_inst)
+                        3'd0: state_nxt = S_ADD;
+                        3'd1: state_nxt = S_SUB;
+                        3'd2: state_nxt = S_AND;
+                        3'd3: state_nxt = S_OR;
+                        3'd4: state_nxt = S_SLT;
+                        3'd5: state_nxt = S_SLL;
+                        3'd6: state_nxt = S_MUL;
+                        3'd7: state_nxt = S_DIV;
+                        default: state_nxt = S_IDLE;
+                    endcase
+                end
+                result_nxt = 0;
+                valid_signal_nxt = 0;
             end
 
-            // S_ADD :
-            // S_SUB :
-            S_AND : begin 
-                state_nxt = S_OUT; 
-                result_nxt = {{32'b0}, operand_a & operand_b};
+            S_ADD : begin
+                state_nxt = S_OUT;
+                if($signed(operand_a) > 0 && $signed(operand_b) > 0) begin
+                    temp = $signed(operand_a) + $signed(operand_b);
+                    result_nxt = (temp[31] == 1) ? {1, {30{1'b1}}} : {33'b0, (operand_a + operand_b)};
+                end
+                else if ($signed(operand_a) < 0 && $signed(operand_b) < 0) begin
+                    temp = $signed(operand_a) + $signed(operand_b);
+                    result_nxt = (temp[31] == 0) ? {33'b0, {1, 31'b0}} : {33'b0, (operand_a + operand_b)};
+                end
+                else begin
+                    result_nxt = {33'b0, operand_a + operand_b};
+                end
                 valid_signal_nxt = 1;
             end
-            // S_OR :
-            // S_SLT :
-            // S_SLL :
-            // S_MUL :
-            // S_DIV :
+
+            S_SUB : begin
+                state_nxt = S_OUT;
+                if($signed(operand_a) > 0 && $signed(operand_b) < 0) begin
+                    temp = $signed(operand_a) + $signed(~operand_b + 1);
+                    result_nxt = (temp[31] == 1) ? {1, {30{1'b1}}} : {33'b0, (operand_a + ~operand_b + 1)};
+                end
+                else if ($signed(operand_a) < 0 && $signed(operand_b) > 0) begin
+                    temp = $signed(operand_a) + $signed(~operand_b + 1);
+                    result_nxt = (temp[31] == 0) ? {33'b0, {1, 31'b0}} : {33'b0, (operand_a + ~operand_b + 1)};
+                end
+                else begin
+                    result_nxt = {33'b0, operand_a + ~operand_b + 1};
+                end
+                valid_signal_nxt = 1;
+            end
+
+            S_AND : begin 
+                state_nxt = S_OUT; 
+                result_nxt = {{33'b0}, operand_a & operand_b};
+                valid_signal_nxt = 1;
+            end
+            S_OR : begin
+                state_nxt = S_OUT;
+                result_nxt = {{33'b0, operand_a | operand_b}};
+                valid_signal_nxt = 1;
+            end
+            S_SLT: begin
+                state_nxt = S_OUT;
+                result_nxt = ($signed(operand_a) < $signed(operand_b)) ? {63'b0, 1'b1} : {63'b0, 1'b0};
+                valid_signal_nxt = 1;
+            end
+            S_SLL : begin
+                state_nxt = S_OUT;
+                result_nxt = {33'b0, operand_a << operand_b};
+                valid_signal_nxt = 1;
+            end
+            S_MUL : begin
+                state_nxt = (counter == 31) ? S_OUT : S_MUL;
+                valid_signal_nxt = (counter == 31) ? 1 : 0;
+                result_nxt = (operand_b[counter]) ? (result + ({33'b0, operand_a} << counter)) : result;
+            end
+            S_DIV : begin
+                state_nxt = (counter == 31) ? S_OUT : S_DIV;
+                valid_signal_nxt = (counter == 31) ? 1 : 0;
+                
+                // if(counter == 31) begin
+                //     result_nxt[64:32] = result[64:32] >>1;
+                //     // $display("%b|%b", result[64:32], result[31:0]);
+                // end
+                // else begin
+                //     result_nxt = result;
+                // end
+
+                if(counter == 0) begin
+                    divTemp = {33'b0, operand_a} << 1;
+                    result_nxt = (divTemp[64:32] < operand_b) ? divTemp << 1 : (({divTemp -  {operand_b, 32'b0}}) << 1) + 1;
+                    // $display("%b|%b", result[64:32], result[31:0]);
+                end
+                else begin
+                    result_nxt = (result[64:32] < operand_b) ? ((state_nxt == S_OUT) ? {result[64:32], result[31:0] << 1}/*result[31:0] << 1 */: result << 1) : ((state_nxt == S_OUT) ? {result[64:32] - operand_b, result[30:0], 1'b1} : (({result -  {operand_b, 32'b0}}) << 1) + 1);
+                    // $display("%b|%b", result[64:32], result[31:0]);
+                end
+
+                
+            end
             S_OUT : begin
                 valid_signal_nxt = 0;
                 state_nxt = S_IDLE;
+                result_nxt = 0;
             end
 
-            default : state_nxt = state;
+            default : begin
+                state_nxt = state;
+                result_nxt = 0;
+                valid_signal_nxt = 0;
+            end
         endcase
     end
     // Todo: Counter
-
-    // Todo: ALU output
+    always @(posedge i_clk or negedge i_rst_n) begin
+            if (!i_rst_n) begin
+                counter <= 0;
+            end 
+            else begin
+                if((state == S_MUL || state == S_DIV) && counter != 31) begin
+                    counter <= counter + 1;
+                end
+                else if(counter == 31) begin
+                    counter <= 0;
+                end
+                else begin
+                    counter <= 0;
+                end
+            end
+    end
     
-    // Todo: output valid signal
 
     // Todo: Sequential always block
     always @(posedge i_clk or negedge i_rst_n) begin
